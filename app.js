4fa7be6c9122e4beba91187e3fab0afccfde5a9a
Nia-art123 <da-alquincy@gmail.com>
2024-09-17 21:39:09 +0100

    first commit

--- app.js
+++ app.js
@@ -18,3 +18,29 @@ function fetchPickUp(){
     jsonData.then(data => pickup.innerText = ` Line : ${data.text}`)
 
 }
+
+function copyText() {
+    
+    const pickupElement = document.getElementById('pickup');
+    const pickupText = pickupElement.innerText.trim();
+
+ 
+    if (pickupText === "") {
+        alert("Can't copy empty text");
+        return;
+    }
+
+   
+    const textarea = document.createElement('textarea');
+    textarea.value = pickupText;
+    document.body.appendChild(textarea);
+    textarea.select();
+    
+    
+    document.execCommand('copy');
+    
+
+    document.body.removeChild(textarea);
+  
+    alert('Pickup line copied to clipboard!');
+}
